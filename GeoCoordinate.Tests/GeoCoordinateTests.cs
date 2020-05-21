10ff82cb0e925167767dd079c99aa55e2b23175b
john <john.demetriou.jd@gmail.com>
2020-05-21 14:11:27 +0300

    Add Unit Tests for 3D position check

--- GeoCoordinate.Tests/GeoCoordinateTests.cs
+++ GeoCoordinate.Tests/GeoCoordinateTests.cs
@@ -28,5 +28,35 @@ namespace StandardGIS.Tests
         {
             Assert.False(new GeoCoordinate(double.NaN, double.NaN).HasPosition());
         }
+
+        [Fact]
+        public void Has3DPosition_ValidCoordinates_True()
+        {
+            Assert.True(new GeoCoordinate(11, 11, 11).Has3DPosition());
+        }
+
+        [Fact]
+        public void Has3DPosition_InValidLat_False()
+        {
+            Assert.False(new GeoCoordinate(double.NaN, 11, 11).Has3DPosition());
+        }
+
+        [Fact]
+        public void Has3DPosition_InValidLon_False()
+        {
+            Assert.False(new GeoCoordinate(11, double.NaN, 11).Has3DPosition());
+        }
+
+        [Fact]
+        public void Has3DPosition_InValidAlt_False()
+        {
+            Assert.False(new GeoCoordinate(11, 11, double.NaN).Has3DPosition());
+        }
+
+        [Fact]
+        public void Has3DPosition_InValidCoordinates_False()
+        {
+            Assert.False(new GeoCoordinate(double.NaN, double.NaN, double.NaN).Has3DPosition());
+        }
     }
 }
