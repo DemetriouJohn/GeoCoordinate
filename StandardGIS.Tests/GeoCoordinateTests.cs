38135bbd4b0409ef30db4a02df1c0370c652bdc1
john <john.demetriou.jd@gmail.com>
2020-05-21 12:36:14 +0300

    Add tests project and first Unit Tests

---
+++ StandardGIS.Tests/GeoCoordinateTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace StandardGIS.Tests
+{
+    public class GeoCoordinateTests
+    {
+        [Fact]
+        public void HasPosition_ValidCoordinates_True()
+        {
+            Assert.True(new GeoCoordinate(11, 11).HasPosition());
+        }
+
+        [Fact]
+        public void HasPosition_InValidLat_False()
+        {
+            Assert.False(new GeoCoordinate(double.NaN, 11).HasPosition());
+        }
+
+        [Fact]
+        public void HasPosition_InValidLon_False()
+        {
+            Assert.False(new GeoCoordinate(11, double.NaN).HasPosition());
+        }
+
+        [Fact]
+        public void HasPosition_InValidCoordinates_False()
+        {
+            Assert.False(new GeoCoordinate(double.NaN, double.NaN).HasPosition());
+        }
+    }
+}
